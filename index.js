5a486dcc54bce47349876a5f1d021b58b90efe5c
mstsurnalyakter <surnalyms@gmail.com>
2024-05-24 17:24:20 +0600

    update code

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ async function run() {
 
 
        //user related
+       app.get("/users", async(req,res)=>{
+        const result = await userCollection.find().toArray();
+        res.send(result);
+       })
        app.post("/users",async(req,res)=>{
         const user = req.body;
         //insert email if user doesn't exists
@@ -50,7 +54,26 @@ async function run() {
         res.send(result);
        })
 
+       app.delete("/users/:id", async(req,res)=>{
+        const result = await userCollection.deleteOne({_id:new ObjectId(req.params.id)});
+        res.send(result);
+       })
 
+       app.patch("/users/admin/:id", async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updateDoc = {
+          $set:{
+            role:"admin"
+          }
+        }
+        const result = await userCollection.updateOne(filter,updateDoc);
+        res.send(result);
+
+       })
+
+
+       // menu related apis
        app.get("/menu", async(req,res)=>{
         const result = await menuCollection.find().toArray();
         res.send(result);
