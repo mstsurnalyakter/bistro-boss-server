d5d118b1f99401fbbf585f2af649dcc9812dd657
mstsurnalyakter <surnalyms@gmail.com>
2024-05-25 07:08:40 +0600

    admin related api added

--- index.js
+++ index.js
@@ -60,10 +60,28 @@ async function run() {
 
        //user related
        app.get("/users",verifyToken, async(req,res)=>{
-
         const result = await userCollection.find().toArray();
         res.send(result);
        })
+
+       app.get("/user/admin/:email",verifyToken ,async(req,res)=>{
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({message:'unauthorized access'})
+        }
+
+        const query = {email};
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === 'admin'
+        }
+
+        res.send({admin})
+
+       })
+
+
        app.post("/users",async(req,res)=>{
         const user = req.body;
         //insert email if user doesn't exists
